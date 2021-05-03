389d7c19859058f97f6e66dbafb8ea3309ce5f75
Ábel Mandl <abelmandl00@gmail.com>
2021-05-03 09:55:26 +0200

    frontend update

--- src/components/Login.js
+++ src/components/Login.js
@@ -44,29 +44,29 @@ export const Login = () => {
     }
   })
   return (
-	<div class="ftco-section">
-		<div class="container">
-			<div class="row justify-content-center">
+	<div className="ftco-section">
+		<div className="container">
+			<div className="row justify-content-center">
 			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-7 col-lg-5">
-					<div class="login-wrap p-4 p-md-5">
-		      	<div class="icon d-flex align-items-center justify-content-center">
-		      		<span class="fa fa-user-o"></span>
+			<div className="row justify-content-center">
+				<div className="col-md-7 col-lg-5">
+					<div className="login-wrap p-4 p-md-5">
+		      	<div className="icon d-flex align-items-center justify-content-center">
+		      		<span className="fa fa-user-o"></span>
 		      	</div>
-		      	<h3 class="text-center mb-4">Sign In</h3>
-						<form action="#" class="login-form">
-		      		<div class="form-group">
-		      			<input type="text" class="form-control rounded-left" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
+		      	<h3 className="text-center mb-4">Sign In</h3>
+						<form action="#" className="login-form">
+		      		<div className="form-group">
+		      			<input type="text" className="form-control rounded-left" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
 		      		</div>
-	            <div class="form-group d-flex">
-	              <input type="password" class="form-control rounded-left" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
+	            <div className="form-group d-flex">
+	              <input type="password" className="form-control rounded-left" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)}/>
 	            </div>
-	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary rounded submit px-3" onClick={login}>Login</button>
+	            <div className="form-group">
+	            	<button type="submit" className="form-control btn btn-primary rounded submit px-3" onClick={login}>Login</button>
 	            </div>
-              <div class="form-group">
-                <button type="submit" class="form-control btn btn-success rounded submit px-3" onClick={pushtoregister}>Sign Up</button>
+              <div className="form-group">
+                <button type="submit" className="form-control btn btn-success rounded submit px-3" onClick={pushtoregister}>Sign Up</button>
 	            </div>
 	          </form>   
 	        </div>
